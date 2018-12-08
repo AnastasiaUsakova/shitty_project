fae5a52fa6f53dc4b8fd46e24c804c474002569c
Your Name <you@example.com>
2018-12-08 19:53:42 +0300

    init

---
+++ src/main/java/ru/study/shitty_project/ShittyProjectApplication.java
@@ -0,0 +1,12 @@
+package ru.study.shitty_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShittyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShittyProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/study/shitty_project/ShittyProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.study.shitty_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShittyProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
